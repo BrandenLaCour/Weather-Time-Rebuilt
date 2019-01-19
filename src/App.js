9c079e664fa37f3612d5a8a93e30f9f4b6fcbccc
Branden LaCour <38292736+BrandenLaCour@users.noreply.github.com>
2019-01-18 22:01:55 -0600

    Remember to branch out next time

--- src/App.js
+++ src/App.js
@@ -13,245 +13,8 @@ import axios from "axios";
 // need to refractor a lot of code on this page into components. Mainly the axios calls.
 
 class App extends Component {
-  state = {
-    week: [
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      },
-      {
-        day: "",
-        high: "",
-        low: "",
-        cast: "",
-        summary: "",
-        dewPoint: "",
-        humidity: "",
-        windSpeed: "",
-        windGust: "",
-        apparentHigh: "",
-        apparentLow: ""
-      }
-    ],
-    location: { name: "", latLng: "41.8781,-87.6298" },
-    nav: "Home",
-    day: ""
-  };
-  // refractor code to shorten this page, export to other components
-  componentWillMount() {
-    //creating the proper week layout depending on the day the app is rendered
-    const newDate = new Date();
-    const day = newDate.getDay();
-    let oldWeek = [...this.state.week];
-    const newWeek = currentWeek(day);
-    const week = oldWeek.map((e, i) => {
-      e.day = newWeek[i];
-      e.high = "";
-      e.low = "";
-      e.cast = "partly-cloudy-day";
-      return e;
-    });
-
-    this.setState({ week });
-  }
-
-  // get the default weather (chicago) from the api
-  async componentDidMount() {
-    const latLng = this.state.location.latLng;
-    const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${latLng}`
-    );
-    console.log(data);
-    const { data: allDays } = data.daily;
-
-    let week = [...this.state.week];
-    //refractor to shorten code here
-    const days = [
-      allDays[0],
-      allDays[1],
-      allDays[2],
-      allDays[3],
-      allDays[4],
-      allDays[5],
-      allDays[6]
-    ];
-
-    const fiveDay = week.map((e, i) => {
-      e.high = Math.round(days[i].temperatureHigh);
-      e.low = Math.round(days[i].temperatureLow);
-      e.cast = days[i].icon;
-      e.day = week[i].day;
-      e.summary = days[i].summary;
-      e.windGust = days[i].windGust;
-      e.windSpeed = days[i].windSpeed;
-      e.apparentHigh = days[i].apparentTemperatureHigh;
-      e.apparentLow = days[i].apparentTemperatureLow;
-      e.dewPoint = days[i].dewPoint;
-      e.humidity = days[i].humidity;
-      return e;
-    });
-
-    week = fiveDay;
-
-    this.setState({ week });
-  }
-
-  handleCityChange = async e => {
-    if (e.keyCode === 13) {
-      const { data } = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?q=${
-          e.target.value
-        }&key=7ca85273b7674386bb62871692d50432`
-      );
-      try {
-        const geo = data.results[0].geometry;
-        const city = data.results[0].components.city;
-        const state = data.results[0].components.state;
-
-        let location = { ...this.state.location };
-        city ? (location.name = `${city}, ${state}`) : (location.name = state);
-        location.latLng = `${geo.lat},${geo.lng}`;
-        this.setState({ location });
-      } catch (err) {
-        console.log(err, "wrong input, or you did not add anything");
-      }
-      console.log(
-        `hoorah! Your weather is set to ${
-          this.state.location.name
-        } at coordinates ${this.state.location.latLng}`
-      );
-      this.updateTest();
-    }
-  };
-  // switch the location to the designated area
-  updateTest = async () => {
-    console.log("ran");
-    const name = this.state.location.name;
-    const latLng = this.state.location.latLng;
-    const { data } = await axios.get(
-      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${latLng}`
-    );
-
-    const { data: allDays } = data.daily;
-
-    let week = [...this.state.week];
-    //refractor to shorten code here
-    const days = [
-      allDays[0],
-      allDays[1],
-      allDays[2],
-      allDays[3],
-      allDays[4],
-      allDays[5],
-      allDays[6]
-    ];
-
-    const fiveDay = week.map((e, i) => {
-      e.high = Math.round(days[i].temperatureHigh);
-      e.low = Math.round(days[i].temperatureLow);
-      e.cast = days[i].icon;
-      e.day = week[i].day;
-      e.summary = days[i].summary;
-      return e;
-    });
-
-    week = fiveDay;
-    let location = { ...this.state.location };
-    location.name = name;
-    const nav = "Weather";
-    this.setState({ week, location, nav });
-  };
-
-  handleDayChoice = day => {
-    this.setState({ day: day });
-  };
-
-  handleHome = nav => {
-    this.setState({ nav });
-  };
-
-  handleReset = () => {
-    const day = "";
-    const nav = "Home";
-    this.setState({ day, nav });
-  };
-
   render() {
-    const { week, location } = this.state;
+    const { week, location } = this.props;
 
     return (
       <div
@@ -259,32 +22,32 @@ class App extends Component {
         className={"container"}
         style={{ marginTop: "100px" }}
       >
-        {this.state.nav === "Home" ? (
+        {this.props.nav === "Home" ? (
           <Change
-            enter={this.handleCityChange}
-            loc={this.updateTest}
+            enter={this.props.handleCityChange}
+            loc={this.props.updateTest}
             label={location.name ? location.name : "Enter City Here"}
           />
-        ) : this.state.nav === "Weather" && !this.state.day ? (
+        ) : this.props.nav === "Weather" && !this.props.day ? (
           <WeatherGrid
             week={week}
             label={location.name}
-            dayNav={this.handleDayChoice}
+            dayNav={this.props.handleDayChoice}
           />
         ) : (
           <SingleDay
             pickedDay={
-              this.state.day
-                ? this.state.week.filter(e => e.day === this.state.day)[0]
+              this.props.day
+                ? this.props.week.filter(e => e.day === this.props.day)[0]
                 : ""
             }
           />
         )}
-        {this.state.nav !== "Home" ? (
+        {this.props.nav !== "Home" ? (
           <div className="row justify-center">
             <div className="col-1" />
             <Button
-              onClick={this.handleReset}
+              onClick={this.props.handleReset}
               variant="contained"
               color="primary"
             >
--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 // @material-ui/icons
-
+import { currentWeek } from "../../components/utils/currentWeek";
 // core components
 import Header from "components/Header/Header.jsx";
 import Footer from "components/Footer/Footer.jsx";
@@ -14,7 +14,7 @@ import GridItem from "components/Grid/GridItem.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
-
+import axios from "axios";
 import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
 
 // Sections for this page
@@ -24,12 +24,236 @@ const dashboardRoutes = [];
 
 class LandingPage extends React.Component {
   state = {
-    nav: false
+    week: [
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      },
+      {
+        day: "",
+        high: "",
+        low: "",
+        cast: "",
+        summary: "",
+        dewPoint: "",
+        humidity: "",
+        windSpeed: "",
+        windGust: "",
+        apparentHigh: "",
+        apparentLow: ""
+      }
+    ],
+    location: { name: "", latLng: "41.8781,-87.6298" },
+    nav: "Home",
+    day: ""
+  };
+  // refractor code to shorten this page, export to other components
+  componentWillMount() {
+    //creating the proper week layout depending on the day the app is rendered
+    const newDate = new Date();
+    const day = newDate.getDay();
+    let oldWeek = [...this.state.week];
+    const newWeek = currentWeek(day);
+    const week = oldWeek.map((e, i) => {
+      e.day = newWeek[i];
+      e.high = "";
+      e.low = "";
+      e.cast = "partly-cloudy-day";
+      return e;
+    });
+
+    this.setState({ week });
+  }
+
+  // get the default weather (chicago) from the api
+  async componentDidMount() {
+    const latLng = this.state.location.latLng;
+    const { data } = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${latLng}`
+    );
+    console.log(data);
+    const { data: allDays } = data.daily;
+
+    let week = [...this.state.week];
+    //refractor to shorten code here
+    const days = [
+      allDays[0],
+      allDays[1],
+      allDays[2],
+      allDays[3],
+      allDays[4],
+      allDays[5],
+      allDays[6]
+    ];
+
+    const fiveDay = week.map((e, i) => {
+      e.high = Math.round(days[i].temperatureHigh);
+      e.low = Math.round(days[i].temperatureLow);
+      e.cast = days[i].icon;
+      e.day = week[i].day;
+      e.summary = days[i].summary;
+      e.windGust = days[i].windGust;
+      e.windSpeed = days[i].windSpeed;
+      e.apparentHigh = days[i].apparentTemperatureHigh;
+      e.apparentLow = days[i].apparentTemperatureLow;
+      e.dewPoint = days[i].dewPoint;
+      e.humidity = days[i].humidity;
+      return e;
+    });
+
+    week = fiveDay;
+
+    this.setState({ week });
+  }
+
+  handleCityChange = async e => {
+    if (e.keyCode === 13) {
+      const { data } = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${
+          e.target.value
+        }&key=7ca85273b7674386bb62871692d50432`
+      );
+      try {
+        const geo = data.results[0].geometry;
+        const city = data.results[0].components.city;
+        const state = data.results[0].components.state;
+
+        let location = { ...this.state.location };
+        city ? (location.name = `${city}, ${state}`) : (location.name = state);
+        location.latLng = `${geo.lat},${geo.lng}`;
+        this.setState({ location });
+      } catch (err) {
+        console.log(err, "wrong input, or you did not add anything");
+      }
+      console.log(
+        `hoorah! Your weather is set to ${
+          this.state.location.name
+        } at coordinates ${this.state.location.latLng}`
+      );
+      this.updateTest();
+    }
+  };
+  // switch the location to the designated area
+  updateTest = async () => {
+    console.log("ran");
+    const name = this.state.location.name;
+    const latLng = this.state.location.latLng;
+    const { data } = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/bab642085489d446f31df424bdf3e506/${latLng}`
+    );
+
+    const { data: allDays } = data.daily;
+
+    let week = [...this.state.week];
+    //refractor to shorten code here
+    const days = [
+      allDays[0],
+      allDays[1],
+      allDays[2],
+      allDays[3],
+      allDays[4],
+      allDays[5],
+      allDays[6]
+    ];
+
+    const fiveDay = week.map((e, i) => {
+      e.high = Math.round(days[i].temperatureHigh);
+      e.low = Math.round(days[i].temperatureLow);
+      e.cast = days[i].icon;
+      e.day = week[i].day;
+      e.summary = days[i].summary;
+      return e;
+    });
+
+    week = fiveDay;
+    let location = { ...this.state.location };
+    location.name = name;
+    const nav = "Weather";
+    this.setState({ week, location, nav });
+  };
+
+  handleDayChoice = day => {
+    this.setState({ day: day });
   };
 
   handleReset = () => {
-    const toggle = !this.state.nav;
-    this.setState({ nav: toggle });
+    const day = "";
+    const nav = "Home";
+    this.setState({ day, nav });
   };
 
   render() {
@@ -40,10 +264,10 @@ class LandingPage extends React.Component {
         <Header
           color="transparent"
           routes={dashboardRoutes}
-          clicked={this.handleReset}
           brand="Material Kit React"
           rightLinks={<HeaderLinks />}
           fixed
+          handleReset={this.handleReset}
           changeColorOnScroll={{
             height: 400,
             color: "white"
@@ -55,15 +279,27 @@ class LandingPage extends React.Component {
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>Weather Time</h1>
-                <h4>Grabbing The Weather Api From Zeus</h4>
+                <h4>Casting Dark Sky's Weather from Zeus</h4>
                 <br />
+                {this.state.nav !== "Home" ? (
+                  <h5>Click a day for more information!</h5>
+                ) : null}
               </GridItem>
             </GridContainer>
           </div>
         </Parallax>
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div>
-            <App home={this.state.nav} reset={this.handleReset} />
+            <App
+              nav={this.state.nav}
+              day={this.state.day}
+              week={this.state.week}
+              location={this.state.location}
+              handleRest={this.handleReset}
+              updateTest={this.updateTest}
+              handleDayChoice={this.handleDayChoice}
+              handleCityChange={this.handleCityChange}
+            />
           </div>
         </div>
         <Footer />
